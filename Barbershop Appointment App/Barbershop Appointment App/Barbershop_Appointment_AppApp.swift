f7a87e6214d9c4ed1abafa7d8a314431eedfbe94
Aleksandar Acev <acev_aleksandar@hotmail.com>
2025-01-27 23:24:31 +0100

    Initial commit: Project setup

---
+++ Barbershop Appointment App/Barbershop Appointment App/Barbershop_Appointment_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barbershop_Appointment_AppApp.swift
+//  Barbershop Appointment App
+//
+//  Created by Aleksandar Acev on 27.1.25.
+//
+
+import SwiftUI
+
+@main
+struct Barbershop_Appointment_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barbershop Appointment App/Barbershop Appointment AppTests/Barbershop_Appointment_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Barbershop_Appointment_AppTests.swift
+//  Barbershop Appointment AppTests
+//
+//  Created by Aleksandar Acev on 27.1.25.
+//
+
+import Testing
+@testable import Barbershop_Appointment_App
+
+struct Barbershop_Appointment_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
